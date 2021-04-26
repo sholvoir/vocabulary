2728c52af66bc117daae4d067bdd1f6fe1b21ae5
sholvoir <shaw.he@hotmail.com>
2021-04-26 16:41:07 -0400

    v0.0.1 release

--- README.md
+++ README.md
@@ -1,1 +1,130 @@
-# vocabulary
+# glossary
+
+本工具使用JavaScript编写, 运行环境为Deno, 请自行到 [Deno官网](https://deno.land/) 下载安装.
+
+if you want to build all data by youself, you need run it use [Deno](https://deno.land/).  
+I strongly recommend you to select one and download it directly.
+
+How to run:
+```bash
+deno run -A mod.ts[ -d][ -r][ name]
+```
+-d: debug.  
+-r: release current version.  
+name: run "name" section only.
+
+## [Ogdens](https://sholvoir.github.io/vocabulary/0.0.1/ogdens_basic_english_vocabulary.txt)
+奥格登基础英语850词 Odgen's BASIC ENGLISH VOCABULARY  
+数据来自 <http://ogden.basic-english.org/words.html>
+
+## [Macmillan](https://sholvoir.github.io/vocabulary/0.0.1/macmillan_defining_vocabulary.txt)
+麦克米伦词典2500释义词汇 Clear Definitions From Macmillan Dictionary  
+数据来自 <https://www.macmillandictionary.com/learn/clear_definitions/>
+
+## [LongmanD](https://sholvoir.github.io/vocabulary/0.0.1/longman_american_defining_vocabulary.txt)
+朗文词典2500释义词汇 The Longman American Defining Vocabulary  
+数据来自 <http://www.longmandictionariesusa.com/res/shared/vocab_definitions.pdf>
+
+## [LongmanC](https://sholvoir.github.io/vocabulary/0.0.1/langman_communication_3000.txt)
+朗文通讯3000词 Longman Communication 3000  
+数据来自 [https://www.lextutor.ca/freq/lists_download/longman_3000_list.pdf](https://www.lextutor.ca/freq/lists_download/longman_3000_list.pdf)
+
+## [VOA](https://sholvoir.github.io/vocabulary/0.0.1/voa_special_english_word_book.txt)
+VOA慢速英语1500基础词汇 VOA Special English Word Book  
+数据来自 [https://simple.wikipedia.org/wiki/Wikipedia:VOA_Special_English_Word_Book](https://simple.wikipedia.org/wiki/Wikipedia:VOA_Special_English_Word_Book)
+
+## [Wik](https://sholvoir.github.io/vocabulary/0.0.1/wiktionary_1000_basic_english_words)
+维基词典1000基础词汇 Appendix: 1000 basic English words  
+数据来自 <https://en.wiktionary.org/wiki/Appendix:1000_basic_English_words>
+
+## [Oxford](https://sholvoir.github.io/vocabulary/0.0.1/oxford_5000.txt)
+牛津5000词汇, Oxford 5000, 分为A1,A2,B1,B2,C1五个级别  
+数据来自 <https://www.oxfordlearnersdictionaries.com/wordlists/>
+
+## [Collins5](https://sholvoir.github.io/vocabulary/0.0.1/collins_5.txt) [Collins4.txt](https://sholvoir.github.io/vocabulary/0.0.1/collins_4.txt) [Collins3.txt](https://sholvoir.github.io/vocabulary/0.0.1/collins_3.txt) [Collins2.txt](https://sholvoir.github.io/vocabulary/0.0.1/collins_2.txt) [Collins1.txt](https://sholvoir.github.io/vocabulary/0.0.1/collins_1.txt)
+柯林斯词典词频分级词汇1-5星  
+数据来自 <https://pan.baidu.com/s/1Eu2kfAIdMwy_mM6Wokllrw> 提取码:2suj
+
+## [NGSL](https://sholvoir.github.io/vocabulary/0.0.1/new_general_service_list.txt) [NGSL-S](https://sholvoir.github.io/vocabulary/0.0.1/new_general_service_list_spoken.txt) [NAWL](https://sholvoir.github.io/vocabulary/0.0.1/new_academic_word_list.txt) [TSL](https://sholvoir.github.io/vocabulary/0.0.1/toeic_word_list.txt) [BSL](https://sholvoir.github.io/vocabulary/0.0.1/business_service_list.txt) [NDL](https://sholvoir.github.io/vocabulary/0.0.1/new_dolch_list.txt) [FEL](https://sholvoir.github.io/vocabulary/0.0.1/fitness_english_list.txt)
+新GSL高频词汇 NEW GENERAL SERVICE LIST (NGSL)  
+新GSL高频词汇口语版 NEW GENERAL SERVICE LIST-SPOKEN (NGSL-S)  
+新学术词汇 NEW ACADEMIC WORD LIST (NAWL)  
+托业词汇 TOEIC SERVICE LIST (TSL)  
+Test of English for International Communication (TOEIC)  
+商业词汇 BUSINESS SERVICE LIST (BSL)  
+新Dolch词汇 NEW DOLCH LIST (NDL)  
+健身词汇 FITNESS ENGLISH LIST (FEL)  
+数据来自 <http://www.newgeneralservicelist.org/>
+
+## [COCA](https://sholvoir.github.io/vocabulary/0.0.1/coca_word_frequency_top_5000.txt)
+当代美国英语语料库 Corpus of Contemporary American English (COCA) top5000  
+数据来自 https://www.wordfrequency.info/samples/wordFrequency.xlsx
+
+## [BNC](https://sholvoir.github.io/vocabulary/0.0.1/bnc_word_frequency_top_5000.txt)
+英国国家语料库 British National Corpus (BNC) top5000  
+数据来自 http://ucrel.lancs.ac.uk/bncfreq/flists.html
+
+## [HSEE-Y](https://sholvoir.github.io/vocabulary/0.0.1/high_school_entrance_examination_youdao.txt) [HSEE-X](https://sholvoir.github.io/vocabulary/0.0.1/high_school_entrance_examination_xindongfang.txt) [NCEE-Y](https://sholvoir.github.io/vocabulary/0.0.1/national_college_entrance_examination_youdao.txt) [NCEE-X](https://sholvoir.github.io/vocabulary/0.0.1/national_college_entrance_examination_xindongfang.txt) [NPEE-YZ](https://sholvoir.github.io/vocabulary/0.0.1/national_postgraduate_entrance_examination_youdao_zhenti.txt) [NPEE-Y](https://sholvoir.github.io/vocabulary/0.0.1/national_postgraduate_entrance_examination_youdao.txt) [NPEE-X](https://sholvoir.github.io/vocabulary/0.0.1/national_postgraduate_entrance_examination_xindongfang.txt) [CET4-YZ](https://sholvoir.github.io/vocabulary/0.0.1/college_english_test_level_4_youdao_zhenti.txt) [CET4-Y](https://sholvoir.github.io/vocabulary/0.0.1/college_english_test_level_4_youdao.txt) [CET4-X](https://sholvoir.github.io/vocabulary/0.0.1/college_english_test_level_4_xindongfang.txt) [CET6-YZ](https://sholvoir.github.io/vocabulary/0.0.1/college_english_test_level_6_youdao_zhenti.txt) [CET6-Y](https://sholvoir.github.io/vocabulary/0.0.1/college_english_test_level_6_youdao.txt) [CET6-X](https://sholvoir.github.io/vocabulary/0.0.1/college_english_test_level_6_xindongfang.txt) [TOEFL-Y](https://sholvoir.github.io/vocabulary/0.0.1/test_of_english_as_a_foreign_language_youdao.txt) [TOEFL-X](https://sholvoir.github.io/vocabulary/0.0.1/test_of_english_as_a_foreign_language_xindongfang.txt) [Ietls-Y](https://sholvoir.github.io/vocabulary/0.0.1/international_english_language_testing_system_youdao.txt) [Ietls-X](https://sholvoir.github.io/vocabulary/0.0.1/international_english_language_testing_system_xindongfang.txt) [SAT](https://sholvoir.github.io/vocabulary/0.0.1/scholastic_assessment_test_xindongfang.txt) [GRE-Y](https://sholvoir.github.io/vocabulary/0.0.1/graduate_record_examinations_youdao.txt) [GRE-X](https://sholvoir.github.io/vocabulary/0.0.1/graduate_record_examinations_xindongfang.txt)  [GMAT-Y](https://sholvoir.github.io/vocabulary/0.0.1/graduate_management_admission_test_youdao.txt) [GMAT-X](https://sholvoir.github.io/vocabulary/0.0.1/graduate_management_admission_test_xindongfang.txt) [BEC](https://sholvoir.github.io/vocabulary/0.0.1/business_environment_and_concepts_section_of_certified_public_accountant_exam_xindongfang.txt)
+中考英语考试大纲 High School Entrance Examination (HSEE)  
+高考英语考试大纲 National College Entrance Examination (CEE)  
+研究生入学考试英语考试大纲 National Postgraduate Entrance Examination (NPEE)  
+大学生英语水平测试4级 College English Test Level 4 (CET4)  
+大学生英语水平测试6级 College English Test Level 6 (CET6)  
+非英语母语英语能力考试(托福) Test of English as a Foreign Language (TOEFL)  
+国际英语测试系统(雅思) International English Language Testing System (IELTS)  
+美国学术评估测试 Scholastic Assessment Test (SAT)  
+美国研究生入学考试 Graduate Record Examinations (GRE)  
+研究生管理科入学考试 Graduate Management Admission Test (GMAT)  
+商业环境与概念注册会计师考试 Business Environment and Concepts (BEC) CPA Exam Section  
+数据来自 https://github.com/kajweb/dict
+
+## vocabulary.txt
+上述所有单词加标签, all words above with tags:
+```JavaScript
+{
+    OG: 'Ogden',
+    MC: 'Macmillan',
+    LD: 'LongmanD',
+    S1: 'LongmanS1',
+    S2: 'LongmanS2',
+    S3: 'LongmanS3',
+    W1: 'LongmanW1',
+    W2: 'LongmanW2',
+    W3: 'LongmanW3',
+    VA: 'VOA1500',
+    WK: 'Wik1000',
+    A1: 'OxfordA1',
+    A2: 'OxfordA2',
+    B1: 'OxfordB1',
+    B2: 'OxfordB2',
+    C1: 'OxfordC1',
+    C2: 'OxfordC2',
+    L1: 'Collins1',
+    L2: 'Collins2',
+    L3: 'Collins3',
+    L4: 'Collins4',
+    L5: 'Collins5',
+    GL: 'NGSL',
+    GS: 'NGSLS',
+    AW: 'NAWL',
+    TL: 'TSL',
+    BL: 'BSL',
+    DL: 'NDL',
+    FE: 'FEL',
+    CA: 'COCA',
+    BN: 'BNC',
+    ZK: 'HSEE',
+    GK: 'NECC',
+    KY: 'NPEE',
+    T4: 'CET4',
+    T6: 'CET6',
+    TF: 'Toefl',
+    IS: 'Ietls',
+    ST: 'SAT',
+    GR: 'GRE',
+    GM: 'GMAT',
+    BE: 'BEC'
+}
+```
+
+欢迎提供更加权威的词表 <sovar.he@gmail.com>，以更新数据版本。
---
+++ mod.ts
@@ -0,0 +1,352 @@
+import { readline } from 'https://deno.land/x/readline/mod.ts';
+import { parse as yamlParse, stringify as yamlStringify } from 'https://deno.land/std@0.92.0/encoding/yaml.ts';
+import { getArgs } from 'https://sholvoir.github.io/args/mod.ts';
+import marked from 'https://ga.jspm.io/npm:marked@2.0.3/lib/marked.esm.js';
+
+const decoder = new TextDecoder();
+const encoder = new TextEncoder();
+const configPath = 'config.yaml';
+const readmePath = 'README.md';
+const vocabularyPath = 'vocabulary.txt';
+let outputDir = 'public/';
+let debug: any;
+let release: any;
+let shouldWriteConfig: any;
+
+interface InputConf {
+    name: string;
+    path: string;
+    conf?: InputConf;
+    tag?: string;
+    wordPath?: Array<string|number>;
+    tagPath?: Array<string|number>;
+    replaces?: Array<[[string, string], string]>;
+    test?: string;
+    separator?: string;
+    revision?: string;
+    output?: string;
+    writeWithTags?: boolean;
+    miss?: string;
+}
+
+interface Config {
+    version: string;
+    inputs: Array<InputConf>;
+}
+
+export class Tags extends Set<string> {
+    static encoder: Record<string, string> = {
+        ogden:      'OG',
+        macmillan:  'MC',
+        longmand:   'LD',
+        longmanS1:  'S1',
+        longmanS2:  'S2',
+        longmanS3:  'S3',
+        longmanW1:  'W1',
+        longmanW2:  'W2',
+        longmanW3:  'W3',
+        voa:        'VA',
+        wik:        'WK',
+        oxfordA1:   'A1',
+        oxfordA2:   'A2',
+        oxfordB1:   'B1',
+        oxfordB2:   'B2',
+        oxfordC1:   'C1',
+        oxfordC2:   'C2',
+        collins1:   'L1',
+        collins2:   'L2',
+        collins3:   'L3',
+        collins4:   'L4',
+        collins5:   'L5',
+        ngsl:       'GL',
+        ngsls:      'GS',
+        nawl:       'AW',
+        tsl:        'TS',
+        bsl:        'BS',
+        ndl:        'DL',
+        fel:        'FE',
+        coca:       'CA',
+        bnc:        'BN',
+        hsee:       'ZK',
+        ncee:       'GK',
+        npee:       'KY',
+        cet4:       'T4',
+        cet6:       'T6',
+        toefl:      'TF',
+        ielts:      'IS',
+        sat:        'ST',
+        gre:        'GR',
+        gmat:       'GM',
+        bec:        'BE'
+    }
+    static decoder: Record<string, string> = {
+        OG: 'Ogden',
+        MC: 'Macmillan',
+        LD: 'LangmanD',
+        S1: 'LangmanS1',
+        S2: 'LangmanS2',
+        S3: 'LangmanS3',
+        W1: 'LangmanW1',
+        W2: 'LangmanW2',
+        W3: 'LangmanW3',
+        VA: 'VOA1500',
+        WK: 'Wik1000',
+        A1: 'OxfordA1',
+        A2: 'OxfordA2',
+        B1: 'OxfordB1',
+        B2: 'OxfordB2',
+        C1: 'OxfordC1',
+        C2: 'OxfordC2',
+        L1: 'Collins1',
+        L2: 'Collins2',
+        L3: 'Collins3',
+        L4: 'Collins4',
+        L5: 'Collins5',
+        GL: 'NGSL',
+        GS: 'NGSLS',
+        AW: 'NAWL',
+        TL: 'TSL',
+        BL: 'BSL',
+        DL: 'NDL',
+        FE: 'FEL',
+        CA: 'COCA',
+        BN: 'BNC',
+        ZK: 'HSEE',
+        GK: 'NECC',
+        KY: 'NPEE',
+        T4: 'CET4',
+        T6: 'CET6',
+        TF: 'Toefl',
+        IS: 'Ietls',
+        ST: 'SAT',
+        GR: 'GRE',
+        GM: 'GMAT',
+        BE: 'BEC'
+    }
+    attach(...tags: Array<string>) {
+        if (tags) for (const tag of tags) this.add(tag);
+    }
+    remove(...tags: Array<string>) {
+        for (const tag of tags) this.delete(tag);
+    }
+    toString() {
+        return Array.from(this).sort().join(',');
+    }
+}
+
+function sort(array: Array<string>) {
+    return array.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+}
+function splitAndDo(text: string, separator = '\n', func: (line: string) => void) {
+    for (let line of text.split(separator)) (line = line.trim()) && func(line);
+}
+async function readAndDo(path: string, func: (line: string) => void) {
+    const file = await Deno.open(path);
+    for await (let lineU of readline(file)) {
+        const line = decoder.decode(lineU).trim();
+        if (line) func(line);
+    }
+    file.close();
+}
+
+async function writeToFile(path: string, words: Iterable<string>) {
+    const file = await Deno.open(path, { write: true, create: true, truncate: true });
+    for (let word of words) if (word = word.trim()) await Deno.write(file.rid, encoder.encode(`${word}\n`));
+    file.close();
+}
+
+async function readConfig(path = configPath) {
+    return yamlParse(await Deno.readTextFile(path)) as Config;
+}
+
+async function writeConfig(config: Config, path = configPath) {
+    await Deno.writeTextFile(path, yamlStringify(config as any, {flowLevel: 3, lineWidth: 640}));
+}
+
+function extractAllWords(text: string, pattern = /([A-Za-z'\.-]+)/g, index = 1) {
+    const words = new Set<string>();
+    for (const match of text.matchAll(pattern)) words.add(match[index]);
+    return words;
+}
+
+function getDictionaryApiFunc(lang: 'US'|'GB') {
+    return async (word: string) => {
+        const entries = await (await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en_${lang}/${encodeURIComponent(word)}`)).json();
+        return Array.isArray(entries) ? entries.map(entry => entry.word as string) : [];
+    }
+}
+
+function getSpellCheckFunc(baseUri: string, regex: RegExp, index = 1) {
+    const entitiesRegex = /&(quot|apos|amp|lt|gt|#(x?\d+));/g
+    const entities: {[key: string]: string} = { quot: '"', apos: "'", amp: '&', lt: '<', gt: '>' };
+    const decodeEntities = (m: string, p1: string, p2: string) => p2 ? String.fromCharCode(+`0${p2}`) : entities[p1];
+    return async (word: string) => {
+        const html = await (await fetch(`${baseUri}${encodeURIComponent(word)}`)).text();
+        return Array.from(html.matchAll(regex)).map(match => match[index].trim().replaceAll(entitiesRegex, decodeEntities));
+    }
+}
+
+class Vocabulary extends Map<string, Tags> {
+    add(word: string, ...tags: Array<string>) {
+        if (this.has(word)) this.get(word)?.attach(...tags);
+        else this.set(word, new Tags(tags));
+    }
+    merge(that: Vocabulary) {
+        for (const [word, tags] of that) this.add(word, ...tags);
+    }
+    addItem(item: string) {
+        const [word, ...tags] = item.split(/[,:] */).map(w => w.trim());
+        if (!tags.length) console.log(`... will add ${word} without tags.`);
+        if (word) this.add(word, ...tags);
+    }
+    remove(word: string, ...tags: Array<string>) {
+        const otags = this.get(word);
+        if (otags) otags.remove(...tags);
+    }
+    removeWordsWithoutTags() {
+        for (const [word, tags] of this) tags.size || this.delete(word);
+    }
+    clearTags(...tags: Array<string>) {
+        for (let [word] of this) this.remove(word, ...tags);
+    }
+    load(text: string, tag?: string, separator?: string) {
+        if (tag) splitAndDo(text, separator, word => this.add(word, tag));
+        else splitAndDo(text, separator, this.addItem.bind(this));
+    }
+    toArray() {
+        return sort(Array.from(this).map(([word, tags]) => `${word}${tags.size ? `: ${tags.toString()}` : ''}`));
+    }
+    toString() {
+        return this.toArray().join('\n');
+    }
+    async readFile(path = vocabularyPath, ignoreTags?: boolean) {
+        try { await readAndDo(path, ignoreTags ? this.add.bind(this) : this.addItem.bind(this)); }
+        catch { await readAndDo('dict.txt', this.add.bind(this)); }
+        return this;
+    }
+    async writeFile(path = vocabularyPath, withTags?: boolean) {
+        await writeToFile(path, withTags ? this.toArray() : sort(Array.from(this.keys())));
+    }
+}
+
+export async function extractAndMerge(conf: InputConf, vocabulary: Vocabulary) {
+    const words = new Vocabulary();
+    // import conf
+    const defaultTag = conf.tag || Tags.encoder[conf.name];
+    if (!conf.wordPath) {
+        let text = await Deno.readTextFile(conf.path);
+        if (conf.replaces) for (const [index, [regex, newstr]] of conf.replaces.entries()) {
+            text = text.replace(new RegExp(...regex), newstr || '');
+            if (debug) await Deno.writeTextFile(conf.path.replace(/.*?([^/]*)$/, `$1-${index}.txt`), text);
+        }
+        if (conf.test && new RegExp(conf.test).test(text)) {
+            console.log(`There is still some special char in ${conf.path}.`);
+            Deno.exit(-1);
+        }
+        words.load(text, defaultTag, conf.separator);
+    } else await readAndDo(conf.path, line => {
+        let word = JSON.parse(line);
+        let tag: any;
+        for (const item of conf.wordPath as Array<string|number>) word = word[item];
+        if (conf.tagPath) (tag = word) && conf.tagPath.forEach(item => tag = tag[item]);
+        else tag = defaultTag;
+        words.add(word, tag);
+    });
+    console.log(`${words.size}, add File ${conf.path}`);
+    // revision
+    if (conf.revision) {
+        for (let line of conf.revision.split('\n')) if (line = line.trim()) {
+            let [oldWord, ...newWords] = line.split(':');
+            let otags
+            if ((oldWord = oldWord.trim()) && (otags = words.get(oldWord))) {
+                words.delete(oldWord);
+                for (let newWord of newWords) (newWord = newWord.trim()) && words.add(newWord, ...otags);
+            }
+            else console.log(`${oldWord} not in words.`);
+        }
+        console.log(`${words.size}, use revision`);
+    }
+    if (debug) return;
+    // SpellCheck
+    const spellCheckFuns = [
+        getDictionaryApiFunc('US'),
+        getDictionaryApiFunc('GB'),
+        getSpellCheckFunc('https://www.merriam-webster.com/dictionary/',
+            /<h1 class="hword">(?:<span.*?>)?(.+?)(?:<\/span>)?<\/h1>/g),
+        getSpellCheckFunc('https://www.collinsdictionary.com/search/?dictCode=english&q=',
+            /<h2 class="h2_entry"><span class="orth">(.+?)<\/span>/g),
+        getSpellCheckFunc('https://www.dictionary.com/browse/',
+            /<h1 data-first-headword="true" class=".+?">(.+?)<\/h1>/g),
+        getSpellCheckFunc('https://www.oxfordlearnersdictionaries.com/search/english/?q=',
+            /<h1.+?>(.+?)(?:<span.+?>.+?<\/span>)?<\/h1>/g)
+    ];
+    const miss: Array<string> = [];
+    for (const word of words.keys()) if (!vocabulary.has(word)) {
+        console.log(`Checking ${word}`);
+        let found = false;
+        const replaces = new Set<string>();
+        try { for (const spellCheckFun of spellCheckFuns) {
+            const entries = await spellCheckFun(word);
+            for (const entry of entries) if (found = (entry === word)) break; else replaces.add(entry);
+            if (found) break;
+        }} catch (e) { console.log(e); }
+        if (!found) miss.push(replaces.size ? `${word} :${Array.from(replaces).join()}` : word) && words.delete(word);
+    }
+    console.log(`Miss ${miss.length} words in ${conf.name}`);
+    shouldWriteConfig ||= miss.length || conf.miss;
+    if (miss.length) conf.miss = sort(miss).join('\n');
+    else if (conf.miss) delete conf.miss;
+    // return
+    words.writeFile(`${outputDir}${conf.output || conf.name}.txt`, conf.writeWithTags);
+    vocabulary.merge(words);
+}
+
+async function test() {
+    console.log(encodeURIComponent('c/o'));
+}
+
+async function run() {
+    const {options, args} = getArgs();
+    if (options.has('-t')) { await test(); return; }
+    const config = await readConfig();
+    (release = options.has('-r')) && (outputDir = `../sholvoir.github.io/vocabulary/${config.version}/`);
+    await Deno.mkdir(outputDir);
+    debug = options.has('-d')
+    const vocabulary = await new Vocabulary().readFile(vocabularyPath, release);
+    for (const input of config.inputs)
+        if (!Deno.args.length || input.name == args[0])
+            await extractAndMerge(input, vocabulary);
+    debug || await vocabulary.writeFile(vocabularyPath, true);
+    if (release) {
+        vocabulary.removeWordsWithoutTags();
+        await vocabulary.writeFile(`${outputDir}vocabulary.txt`, true);
+        const cmds = [{
+            cmd: ["git", "add", "."],
+            cwd: "../sholvoir.github.io",
+        }, {
+            cmd: ["git", "commit", "-m", `"vocabulary ${config.version}"`],
+            cwd: "../sholvoir.github.io",
+        }, {
+            cmd: ["git", "push"],
+            cwd: "../sholvoir.github.io",
+        }];
+        if (!debug) for (const cmd of cmds) {
+            const p = Deno.run(cmd);
+            const r = await p.status();
+            if (!r.success) {
+                console.log(`Failed at "${cmd.cmd.join(" ")}"`);
+                Deno.exit(-1);
+            }
+            p.close();
+        }
+        const readme = (await Deno.readTextFile(readmePath))
+            .replaceAll(new RegExp('(https://sholvoir.github.io/vocabulary/)\\d+\\.\\d+\\.\\d+', 'g'), `$1${config.version}`);
+        await Deno.writeTextFile(readmePath, readme);
+        await Deno.writeTextFile('../sholvoir.github.io/vocabulary/index.html', marked(readme));
+        const ver = config.version.split('.').map(x => parseInt(x));
+        ver[2]++;
+        shouldWriteConfig = config.version = ver.join('.');
+    }
+    shouldWriteConfig && await writeConfig(config);
+}
+await run();
