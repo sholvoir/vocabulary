2c218ebdf468c000617ef064d01cbf3d11730759
sholvoir <shaw.he@hotmail.com>
2021-05-06 11:30:16 -0400

    int tag

--- mod.ts
+++ mod.ts
@@ -1,5 +1,6 @@
 import { readline } from 'https://deno.land/x/readline/mod.ts';
 import { parse as yamlParse, stringify as yamlStringify } from 'https://deno.land/std@0.92.0/encoding/yaml.ts';
+import { Tag } from 'https://sholvoir.github.io/generic/word_tag.ts';
 import getArgs from 'https://sholvoir.github.io/generic/args.ts';
 import marked from 'https://ga.jspm.io/npm:marked@2.0.3/lib/marked.esm.js';
 import publish from 'https://sholvoir.github.io/generic/publish.ts';
@@ -18,7 +19,7 @@ interface InputConf {
     name: string;
     path: string;
     conf?: InputConf;
-    tag?: string;
+    tag?: bigint;
     wordPath?: Array<string|number>;
     tagPath?: Array<string|number>;
     replaces?: Array<[[string, string], string]>;
@@ -33,106 +34,6 @@ interface InputConf {
 interface Config {
     version: string;
     inputs: Array<InputConf>;
-}
-
-export class Tags extends Set<string> {
-    static encoder: Record<string, string> = {
-        ogden:      'OG',
-        macmillan:  'MC',
-        longmand:   'LD',
-        longmanS1:  'S1',
-        longmanS2:  'S2',
-        longmanS3:  'S3',
-        longmanW1:  'W1',
-        longmanW2:  'W2',
-        longmanW3:  'W3',
-        voa:        'VA',
-        wik:        'WK',
-        oxfordA1:   'A1',
-        oxfordA2:   'A2',
-        oxfordB1:   'B1',
-        oxfordB2:   'B2',
-        oxfordC1:   'C1',
-        oxfordC2:   'C2',
-        collins1:   'L1',
-        collins2:   'L2',
-        collins3:   'L3',
-        collins4:   'L4',
-        collins5:   'L5',
-        ngsl:       'GL',
-        ngsls:      'GS',
-        nawl:       'AW',
-        tsl:        'TS',
-        bsl:        'BS',
-        ndl:        'DL',
-        fel:        'FE',
-        coca:       'CA',
-        bnc:        'BN',
-        hsee:       'ZK',
-        ncee:       'GK',
-        npee:       'KY',
-        cet4:       'T4',
-        cet6:       'T6',
-        toefl:      'TF',
-        ielts:      'IS',
-        sat:        'ST',
-        gre:        'GR',
-        gmat:       'GM',
-        bec:        'BE'
-    }
-    static decoder: Record<string, string> = {
-        OG: 'Ogden',
-        MC: 'Macmillan',
-        LD: 'LangmanD',
-        S1: 'LangmanS1',
-        S2: 'LangmanS2',
-        S3: 'LangmanS3',
-        W1: 'LangmanW1',
-        W2: 'LangmanW2',
-        W3: 'LangmanW3',
-        VA: 'VOA1500',
-        WK: 'Wik1000',
-        A1: 'OxfordA1',
-        A2: 'OxfordA2',
-        B1: 'OxfordB1',
-        B2: 'OxfordB2',
-        C1: 'OxfordC1',
-        C2: 'OxfordC2',
-        L1: 'Collins1',
-        L2: 'Collins2',
-        L3: 'Collins3',
-        L4: 'Collins4',
-        L5: 'Collins5',
-        GL: 'NGSL',
-        GS: 'NGSLS',
-        AW: 'NAWL',
-        TL: 'TSL',
-        BL: 'BSL',
-        DL: 'NDL',
-        FE: 'FEL',
-        CA: 'COCA',
-        BN: 'BNC',
-        ZK: 'HSEE',
-        GK: 'NECC',
-        KY: 'NPEE',
-        T4: 'CET4',
-        T6: 'CET6',
-        TF: 'Toefl',
-        IS: 'Ietls',
-        ST: 'SAT',
-        GR: 'GRE',
-        GM: 'GMAT',
-        BE: 'BEC'
-    }
-    attach(...tags: Array<string>) {
-        if (tags) for (const tag of tags) this.add(tag);
-    }
-    remove(...tags: Array<string>) {
-        for (const tag of tags) this.delete(tag);
-    }
-    toString() {
-        return Array.from(this).sort().join(',');
-    }
 }
 
 function sort(array: Array<string>) {
@@ -157,7 +58,9 @@ async function writeToFile(path: string, words: Iterable<string>) {
 }
 
 async function readConfig(path = configPath) {
-    return yamlParse(await Deno.readTextFile(path)) as Config;
+    const config = yamlParse(await Deno.readTextFile(path)) as Config;
+    for (const input of config.inputs) input.tag && (input.tag = BigInt(input.tag))
+    return 
 }
 
 async function writeConfig(config: Config, path = configPath) {
@@ -187,53 +90,54 @@ function getSpellCheckFunc(baseUri: string, regex: RegExp, index = 1) {
     }
 }
 
-class Vocabulary extends Map<string, Tags> {
-    add(word: string, ...tags: Array<string>) {
-        if (this.has(word)) this.get(word)?.attach(...tags);
-        else this.set(word, new Tags(tags));
+class Vocabulary extends Map<string, Tag> {
+    add(word: string, tag = 0n) {
+        this.has(word) ? this.get(word)?.attach(tag) : this.set(word, new Tag(tag));
     }
     merge(that: Vocabulary) {
-        for (const [word, tags] of that) this.add(word, ...tags);
+        for (const [word, tag] of that) this.add(word, tag.value);
     }
-    addItem(item: string) {
+    addItem(item: string, tag?: bigint) {
         const [word, ...tags] = item.split(/[,:] */).map(w => w.trim());
-        if (!tags.length) console.log(`... will add ${word} without tags.`);
-        if (word) this.add(word, ...tags);
+        if (tag === undefined) {
+            tag = 0n;
+            for (const t of tags) tag |= (isNaN(<any>t) ? Tag.get(t) || 0n : BigInt(t));
+            if (!tag) console.log(`... will add ${word} without tags.`);
+        }
+        if (word) this.add(word, tag);
     }
-    remove(word: string, ...tags: Array<string>) {
-        const otags = this.get(word);
-        if (otags) otags.remove(...tags);
+    remove(word: string, tag: bigint) {
+        this.get(word)?.remove(tag);
     }
-    removeWordsWithoutTags() {
-        for (const [word, tags] of this) tags.size || this.delete(word);
+    removeWordsWithoutTag() {
+        for (const [word, tag] of this) tag.value || this.delete(word);
     }
-    clearTags(...tags: Array<string>) {
-        for (let [word] of this) this.remove(word, ...tags);
+    clearTag(tag: bigint) {
+        for (let [word] of this) this.remove(word, tag);
     }
-    load(text: string, tag?: string, separator?: string) {
-        if (tag) splitAndDo(text, separator, word => this.add(word, tag));
-        else splitAndDo(text, separator, this.addItem.bind(this));
+    load(text: string, tag?: bigint, separator?: string) {
+        splitAndDo(text, separator, line => this.addItem(line, tag));
     }
     toArray() {
-        return sort(Array.from(this).map(([word, tags]) => `${word}${tags.size ? `: ${tags.toString()}` : ''}`));
+        return Array.from(this).map(([word, tag]) => `${word}${tag.value ? `: ${tag.value}` : ''}`);
     }
     toString() {
-        return this.toArray().join('\n');
+        return sort(this.toArray()).join('\n');
     }
-    async readFile(path = vocabularyPath, ignoreTags?: boolean) {
-        try { await readAndDo(path, ignoreTags ? this.add.bind(this) : this.addItem.bind(this)); }
+    async readFile(path = vocabularyPath, ignoreTag?: boolean) {
+        try { await readAndDo(path, ignoreTag ? line => this.addItem(line, 0n) : this.addItem.bind(this)); }
         catch { await readAndDo('dict.txt', this.add.bind(this)); }
         return this;
     }
     async writeFile(path = vocabularyPath, withTags?: boolean) {
-        await writeToFile(path, withTags ? this.toArray() : sort(Array.from(this.keys())));
+        await writeToFile(path, sort(withTags ? this.toArray() : Array.from(this.keys())));
     }
 }
 
 export async function extractAndMerge(conf: InputConf, vocabulary: Vocabulary) {
     const words = new Vocabulary();
     // import conf
-    const defaultTag = conf.tag || Tags.encoder[conf.name];
+    const defaultTag = conf.tag || Tag.get(conf.name);
     if (!conf.wordPath) {
         let text = await Deno.readTextFile(conf.path);
         if (conf.replaces) for (const [index, [regex, newstr]] of conf.replaces.entries()) {
@@ -247,10 +151,13 @@ export async function extractAndMerge(conf: InputConf, vocabulary: Vocabulary) {
         words.load(text, defaultTag, conf.separator);
     } else await readAndDo(conf.path, line => {
         let word = JSON.parse(line);
-        let tag: any;
         for (const item of conf.wordPath as Array<string|number>) word = word[item];
-        if (conf.tagPath) (tag = word) && conf.tagPath.forEach(item => tag = tag[item]);
-        else tag = defaultTag;
+        let tag: any = defaultTag;
+        if (conf.tagPath) {
+            tag = word;
+            for (const i of conf.tagPath) tag = tag[i];
+            tag = +tag;
+        }
         words.add(word, tag);
     });
     console.log(`${words.size}, add File ${conf.path}`);
@@ -258,10 +165,10 @@ export async function extractAndMerge(conf: InputConf, vocabulary: Vocabulary) {
     if (conf.revision) {
         for (let line of conf.revision.split('\n')) if (line = line.trim()) {
             let [oldWord, ...newWords] = line.split(':');
-            let otags
-            if ((oldWord = oldWord.trim()) && (otags = words.get(oldWord))) {
+            if (words.has(oldWord = oldWord.trim())) {
+                const otag = words.get(oldWord)?.value || 0n;
                 words.delete(oldWord);
-                for (let newWord of newWords) (newWord = newWord.trim()) && words.add(newWord, ...otags);
+                for (let newWord of newWords) (newWord = newWord.trim()) && words.add(newWord, otag);
             }
             else console.log(`${oldWord} not in words.`);
         }
@@ -298,7 +205,7 @@ export async function extractAndMerge(conf: InputConf, vocabulary: Vocabulary) {
     if (miss.length) conf.miss = sort(miss).join('\n');
     else if (conf.miss) delete conf.miss;
     // return
-    words.writeFile(`${outputDir}${conf.output || conf.name}.txt`, conf.writeWithTags);
+    words.writeFile(`${outputDir}${conf.output || `${conf.name}.txt`}`, conf.writeWithTags);
     vocabulary.merge(words);
 }
 
@@ -306,7 +213,8 @@ async function run() {
     const {options, args} = getArgs();
     const config = await readConfig();
     (release = options.has('-r')) && (outputDir = `../sholvoir.github.io/vocabulary/${config.version}/`);
-    await Deno.mkdir(outputDir);
+    try { await Deno.mkdir(outputDir); }
+    catch {}
     debug = options.has('-d')
     const vocabulary = await new Vocabulary().readFile(vocabularyPath, release);
     for (const input of config.inputs)
@@ -314,7 +222,7 @@ async function run() {
             await extractAndMerge(input, vocabulary);
     debug || await vocabulary.writeFile(vocabularyPath, true);
     if (release) {
-        vocabulary.removeWordsWithoutTags();
+        vocabulary.removeWordsWithoutTag();
         await vocabulary.writeFile(`${outputDir}vocabulary.txt`, true);
         const readme = (await Deno.readTextFile(readmePath))
             .replaceAll(new RegExp('(https://sholvoir.github.io/vocabulary/)\\d+\\.\\d+\\.\\d+', 'g'), `$1${config.version}`);
@@ -327,4 +235,9 @@ async function run() {
     }
     shouldWriteConfig && await writeConfig(config);
 }
-await run();
+// await run();
+function test() {
+    let x = yamlParse(`{a: +32578493578291537291374583947892348578293874875328983748n}`);
+    console.log(typeof (<any>x).a, ' ', (<any>x).a);
+}
+test()
