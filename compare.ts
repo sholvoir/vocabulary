b7b075fdf92193fb49d35b3703c97d4e17222387
sholvoir <sovar.he@gmail.com>
2025-01-20 23:14:38 -0500

    0.2.34

--- compare.ts
+++ compare.ts
@@ -2,20 +2,23 @@
 const run = async () => {
     const delimiter = /[,:] */;
     const oldVocabulary = new Set<string>();
-    for (let line of (await Deno.readTextFile(Deno.args[0] ?? 'vocabulary1.txt')).split('/n'))
+    for (let line of (await Deno.readTextFile(Deno.args[0] ?? 'vocabulary1.txt')).split('\n'))
         if (line = line.trim()) {
             let [word] = line.split(delimiter);
             if (word = word.trim()) oldVocabulary.add(word);
         }
     const newVocabulary = new Set<string>();
-    for (let line of (await Deno.readTextFile(Deno.args[1] ?? 'vocabulary.txt')).split('/n'))
+    for (let line of (await Deno.readTextFile(Deno.args[1] ?? 'vocabulary.txt')).split('\n'))
         if (line = line.trim()) {
             let [word] = line.split(delimiter);
             if (word = word.trim()) newVocabulary.add(word);
         }
     for (const word of oldVocabulary) newVocabulary.delete(word);
-    console.log('adding...');
-    for (const word of Array.from(newVocabulary).sort()) console.log(word);
+    if (newVocabulary.size) {
+        console.log('adding...');
+        for (const word of Array.from(newVocabulary).sort()) console.log(word);
+    }
+    console.log('done!');
 }
 
 if (import.meta.main) run();
