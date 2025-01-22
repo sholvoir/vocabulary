84d160470ec2991c52f5a32bd4af75b8b9f2941e
sholvoir <sovar.he@gmail.com>
2025-01-22 16:16:27 -0500

    0.2.49

--- src/spell-check.ts
+++ src/spell-check.ts
@@ -3,24 +3,27 @@ const markRegex = /<.*?>/g;
 const spliteNum = /^([A-Za-zèé /&''.-]+)(\d*)/;
 const entities: Record<string, string> = { quot: '"', apos: "'", amp: '&', lt: '<', gt: '>' };
 const decodeEntities = (_: string, p1: string, p2: string) => p2 ? String.fromCharCode(+`0${p2}`) : entities[p1];
-const getScfunc = (baseUri: string, regex: RegExp, index = 1) => async (word: string) => {
+const reqInit = { headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'} }
+
+const getScfunc = (baseUri: string, regexes: Array<[RegExp, number]>) => async (word: string) => {
     try {
-        const html = await (await fetch(`${baseUri}${encodeURIComponent(word)}`)).text();
-        return Array.from(html.matchAll(regex)).map(match =>
-            match[index].trim().replaceAll(entitiesRegex, decodeEntities).replaceAll(markRegex, '')
-        );
-    } catch (e) {
-        console.log(e);
-        return [];
-    }
+        const result = [];
+        const html = await (await fetch(`${baseUri}${encodeURIComponent(word)}`, reqInit)).text();
+        for (const [regex, index] of regexes)
+            for (const match of html.matchAll(regex))
+                result.push(match[index??1].trim().replaceAll(entitiesRegex, decodeEntities).replaceAll(markRegex, ''));
+        return result;
+    } catch (e) { return (console.log(e), []);}
 };
 const scfuncs = [
-    getScfunc('https://www.merriam-webster.com/dictionary/',
-        /<(?:h1|p) class="hword">(?:<span.*?>)?(.+?)(?:<\/span>)?<\/(?:h1|p)>/g),
-    getScfunc('https://www.collinsdictionary.com/dictionary/english/',
-        /<h2 class="h2_entry"><span class="orth">(.+?)<\/span>/g),
+    /*getScfunc('https://www.merriam-webster.com/dictionary/',
+        [[/<(?:h1|p) class="hword">(?:<span.*?>)?(.+?)(?:<\/span>)?<\/(?:h1|p)>/g, 1],
+         [/<span class="fw-bold ure">(.+?)<\/span>/g, 1],
+         [/<span id=".*?" class="va">(.+?)<\/span>/g, 1]]),*/
+    getScfunc('https://www.oxfordlearnersdictionaries.com/us/definition/english/',
+        [[/<h1 class="headword".*?>(.+?)<\/h1>/g, 1]])/*,
     getScfunc('https://www.dictionary.com/browse/',
-        /(?:<h1>|<span class="c8xVkuOkmPmaouCkeaau">)(.+?)(?:<\/h1>|<\/span>)/g)
+        [[/<(p|h1) class="(?:elMfuCTjKMwxtSEEnUsi)?">(.*?)<\/\1>/g, 2]])*/
 ];
 let functionIndex = 0;
 export const spellCheck = async (word: string): Promise<string[]|undefined> => {
@@ -45,4 +48,7 @@ export const spellCheck = async (word: string): Promise<string[]|undefined> => {
     return Array.from(replaces);
 }
 
-if (import.meta.main) console.log(await spellCheck(Deno.args[0]));
+if (import.meta.main) {
+    functionIndex = +(Deno.args[1] ?? 0);
+    console.log(await spellCheck(Deno.args[0]));
+}
