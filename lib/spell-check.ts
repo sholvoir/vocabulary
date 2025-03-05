8b8674bf1dff50dd998bca498f73999f61fc985b
sholvoir <sovar.he@gmail.com>
2025-03-05 15:44:31 -0500

    0.4.4

--- lib/spell-check.ts
+++ lib/spell-check.ts
@@ -1,67 +1,71 @@
 // deno-lint-ignore-file no-cond-assign no-empty
-const spellCheckIgnoreFile = `${import.meta.dirname}/../spell-check-ignore.txt`;
-const spellCheckFile = `${import.meta.dirname}/../spell-check.txt`;
+const vocabFile = `${import.meta.dirname}/../vocabulary.txt`;
 const entitiesRegex = /&(quot|apos|amp|lt|gt|#(x?\d+));/g;
 const markRegex = /<.*?>/g;
 // const spliteNum = /^([A-Za-zèé /&''.-]+)(\d*)/;
-const entities: Record<string, string> = { quot: '"', apos: "'", amp: '&', lt: '<', gt: '>' };
-const decodeEntities = (_: string, p1: string, p2: string) => p2 ? String.fromCharCode(+`0${p2}`) : entities[p1];
-const reqInit = { headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'} }
-
-const getScfunc = (baseUri: string, regexes: Array<[RegExp, number]>) => async (word: string) => {
-    try {
-        const result = [];
-        const html = await (await fetch(`${baseUri}${encodeURIComponent(word)}`, reqInit)).text();
-        for (const [regex, index] of regexes)
-            for (const match of html.matchAll(regex))
-                result.push(match[index??1].trim().replaceAll(entitiesRegex, decodeEntities).replaceAll(markRegex, ''));
-        return result;
-    } catch (e) { return (console.log(e), []);}
+const agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0';
+const entities: Record<string, string> = {
+    quot: '"', apos: "'", amp: '&', lt: '<', gt: '>'
 };
+const decodeEntities = (_: string, p1: string, p2: string) =>
+    p2 ? String.fromCharCode(+`0${p2}`) : entities[p1];
+const reqInit = { headers: { 'User-Agent': agent } }
+
+const getScfunc = (baseUri: string, regexes: Array<[RegExp, number]>) =>
+    async (word: string) => {
+        try {
+            const result = [];
+            const res = await fetch(`${baseUri}${encodeURIComponent(word)}`, reqInit);
+            const html = await res.text();
+            for (const [regex, index] of regexes)
+                for (const match of html.matchAll(regex))
+                    result.push(match[index ?? 1].trim()
+                        .replaceAll(entitiesRegex, decodeEntities)
+                        .replaceAll(markRegex, ''));
+            return result;
+        } catch (e) { return (console.log(e), []); }
+    };
 
 const scfuncs = [
     getScfunc('https://www.merriam-webster.com/dictionary/',
         [[/<(?:h1|p) class="hword">(?:<span.*?>)?(.+?)(?:<\/span>)?<\/(?:h1|p)>/g, 1],
-         [/<span class="fw-bold ure">(.+?)<\/span>/g, 1],
-         [/<span id=".*?" class="va">(.+?)<\/span>/g, 1]]),
+        [/<span class="fw-bold ure">(.+?)<\/span>/g, 1],
+        [/<span id=".*?" class="va">(.+?)<\/span>/g, 1]]),
     getScfunc('https://www.oxfordlearnersdictionaries.com/us/search/english/?q=',
         [[/<h1 class="headword".*?>(.+?)<\/h1>/g, 1]]),
     getScfunc('https://www.dictionary.com/browse/',
         [[/<(p|h1) class="(?:elMfuCTjKMwxtSEEnUsi)?">(.*?)<\/\1>/g, 2]])
 ];
 
-const spellCheckIgnoreSet = new Set<string>();
-const spellCheckSet = new Set<string>();
+const vocabSet = new Set<string>();
+let vocabSize = 0;
 export const spellCheckInit = async () => {
-    if (!spellCheckIgnoreSet.size) try {
-        for (let line of (await Deno.readTextFile(spellCheckIgnoreFile)).split('\n'))
-            if (line = line.trim()) spellCheckIgnoreSet.add(line);
-    } catch {}
-    if (!spellCheckSet.size) try {
-        for (let line of (await Deno.readTextFile(spellCheckFile)).split('\n'))
-            if (line = line.trim()) spellCheckSet.add(line);
-    } catch {}
+    if (!vocabSet.size) try {
+        for (let line of (await Deno.readTextFile(vocabFile)).split('\n'))
+            if (line = line.trim()) vocabSet.add(line);
+    } catch { }
+    vocabSize = vocabSet.size;
 }
 
 let functionIndex = 0;
-export const spellCheck = async (word: string): Promise<string[]|undefined> => {
+export const spellCheck = async (word: string): Promise<string[] | undefined> => {
     word = word.trim();
-    if (!spellCheckSet) await spellCheckInit();
-    if (spellCheckIgnoreSet.has(word)) return;
-    if (spellCheckSet.has(word)) return;
+    if (!vocabSet) await spellCheckInit();
+    if (vocabSet.has(word)) return;
     console.log(`Spell Check ${word}...`);
     const replaces = new Set<string>();
     for (let i = 0; i < scfuncs.length; i++) {
         const funIndex = functionIndex++ % scfuncs.length;
         const entries = await scfuncs[funIndex](word);
-        if (entries.includes(word)) { spellCheckSet.add(word); return; }
+        if (entries.includes(word)) { vocabSet.add(word); return; }
         else entries.forEach(entry => replaces.add(entry));
     }
     return Array.from(replaces);
 }
 
 export const spellCheckClose = async () => {
-    if (spellCheckSet) await Deno.writeTextFile(spellCheckFile, Array.from(spellCheckSet).sort().join('\n'));
+    if (vocabSet.size > vocabSize)
+        await Deno.writeTextFile(vocabFile, Array.from(vocabSet).sort().join('\n'));
 }
 
 if (import.meta.main) {
