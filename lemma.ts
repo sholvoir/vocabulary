327d51c07e73898ae78b2783696a3628f322e952
sholvoir <sovar.he@gmail.com>
2026-05-06 23:19:49 -0400

    0.6.13

--- lemma.ts
+++ lemma.ts
@@ -3,7 +3,7 @@ import { getHash } from "@sholvoir/generic/hash";
 const run  = async () => {
    const lema = await Deno.readTextFile("docs/lemmatization.yaml");
    const checksumLema = await getHash(lema);
-   await Deno.writeTextFile("docs/checksum-lema.json", JSON.stringify({ checksum: checksumLema }));
+   await Deno.writeTextFile("docs/checksum-lemma.json", JSON.stringify({ checksum: checksumLema }));
 }
 
 if (import.meta.main) await run();
